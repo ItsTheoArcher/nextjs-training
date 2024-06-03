7c7a71ba12e0f66f09799dc1b0beb103e0c544b5
Theo Archer <theo@theoarcher.me>
2024-06-03 18:53:18 +1200

    Add Analytics + update about data

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,21 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
-import { ServerThemeProvider } from "next-themes";
-import Providers from "./providers";
 import { generalData } from "@/data/general";
+import { Analytics } from "@vercel/analytics/react"
 import type { Metadata } from "next";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: `${generalData.name} - ${generalData.jobTitle}`,
-  description: generalData.about,
+  description: generalData.about[0],
   openGraph: {
     type: "website",
     locale: "en_US",
     url: "",
     siteName: `${generalData.name} - ${generalData.jobTitle}`,
     title: `${generalData.name} - ${generalData.jobTitle}`,
-    description: generalData.about,
+    description: generalData.about[0],
     images: [
       {
         url: "",
@@ -29,17 +28,16 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
-  children,
+  children
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <ServerThemeProvider attribute="class">
       <html lang="en">
         <body className={`${inter.className} dark:bg-neutral-900`}>
-          <Providers>{children}</Providers>
+          {children}
+          <Analytics/>
         </body>
       </html>
-    </ServerThemeProvider>
   );
 }
--- data/general.ts
+++ data/general.ts
@@ -3,8 +3,9 @@ export const generalData = {
   avatar: "/images/profile.jpg",
   jobTitle: "Full-stack Developer",
   website: "https://www.linkedin.com/in/theoarcher/",
-  about:
-    "Hey there! I'm Theo, a full-stack developer with significant experience working on enterprise software projects in eCommerce.",
+  about:[ "Hey! I'm Theo, a full-stack developer with significant experience working on enterprise software projects in eCommerce.",
+  "I have great people skills, and a well-rounded background in software development, having worked in a number of developer adjacent roles.",
+  "I take pride in my work, and love working with others to solve problems."],
   contacts: [
     {
       label: "Email",
