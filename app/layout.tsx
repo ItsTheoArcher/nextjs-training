7316850a05c90a1db9388897890b9c9f6bcf6cce
Theo Archer <theo@theoarcher.me>
2024-06-05 20:19:29 +1200

    Resolve darkmode text colour issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import { generalData } from "@/data/general";
 import { Analytics } from "@vercel/analytics/react"
+import { ServerThemeProvider } from "next-themes";
+import Providers from "./providers";
 import type { Metadata } from "next";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -33,11 +35,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
+    <ServerThemeProvider attribute="class">
       <html lang="en">
-        <body className={`${inter.className}`}>
-          {children}
-          <Analytics/>
+        <body className={`${inter.className} dark:bg-neutral-600`}>
+          <Providers>{children}</Providers>
+          <Analytics />
         </body>
       </html>
+    </ServerThemeProvider>
   );
 }
--- components/roles.tsx
+++ components/roles.tsx
@@ -8,21 +8,21 @@ const Roles: React.FC<Institution> = ({ roles }) => {
           {roles.map((role, index) => {
             return (
             <li key={index}>
-              { index !== 0 ? (<hr/>) : null}
+              { index !== 0 ? (<hr className="bg-zinc-200"/>) : null}
               <div className="timeline-middle mx-4">
                 <div className="w-3 h-3 bg-zinc-400 rounded-full"></div>
               </div>
               <div className="timeline-end">
                 <div className="flex flex-col mb-4">
-                  <h4 className="font-semibold text-slate-600">{role.title}</h4>
-                  <div className="text-slate-600">
+                  <h4 className="font-semibold text-slate-600 dark:text-gray-400">{role.title}</h4>
+                  <div className="text-slate-600 dark:text-slate-400">
                     {new Date(role.startDate).toLocaleString('en-nz',{month:'short', year:'numeric'})} - { role.endDate ? new Date(role.endDate).toLocaleString('en-nz',{month:'short', year:'numeric'}) : 'Present'}
                   </div>
-                  <p className="text-slate-600">
+                  <p className="text-slate-600 dark:text-gray-400">
                     {role.subTitle}
                   </p>
                   {role.description ? (
-                    <p className="text-slate-800 mt-2">
+                    <p className="text-slate-800 dark:text-gray-400 mt-2">
                       {role.description}
                     </p>
                   ) : null}
@@ -31,7 +31,7 @@ const Roles: React.FC<Institution> = ({ roles }) => {
                   ) : null}
                 </div>
               </div>
-              { index !== roles.length -1 ? (<hr/>) : null}
+              { index !== roles.length -1 ? (<hr className="bg-zinc-200"/>) : null}
             </li>
             );
           })}
